501cfbf8039b73a1dd87faaa2f81bb57b2a0947d
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-01-16 22:03:53 +0530

    first commit

---
+++ src/main/java/io/rescrypt/book/my/show/BookMyShowApplication.java
@@ -0,0 +1,13 @@
+package io.rescrypt.book.my.show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowApplication.class, args);
+	}
+
+}
